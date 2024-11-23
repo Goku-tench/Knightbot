4aa763c13326474d623ac182cbc584d1513f449b
Goku-tench <godblessisdori9@gmail.com>
2024-11-23 12:08:51 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'My Sticker Pack',
   author: 'My Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Professor', //Goku
+  ownerNumber: '917023951514', //255613667586
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
